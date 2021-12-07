3f3eda9fe6b05cc60709c325d9e59d8612cbf513
febianbatubara <febianbatubaraa@gmail.com>
2021-12-07 18:26:51 +0700

    Add OTP resend timer and reset password flow logic

--- app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/ui/forgotpassword/CreateNewPasswordFragment.kt
+++ app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/ui/forgotpassword/CreateNewPasswordFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.grup3.alodokter_rakamin_android_grup3.base.BaseFragment
 import com.grup3.alodokter_rakamin_android_grup3.databinding.FragmentCreateNewPasswordBinding
 
@@ -18,5 +19,13 @@ class CreateNewPasswordFragment : BaseFragment<FragmentCreateNewPasswordBinding>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnSavePassword.setOnClickListener {
+            Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show()
+
+            // TODO: Send password to API and navigate to login page
+        }
+        binding.btnBack.setOnClickListener {
+            activity?.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/ui/forgotpassword/ForgotPasswordFragment.kt
+++ app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/ui/forgotpassword/ForgotPasswordFragment.kt
@@ -1,6 +1,7 @@
 package com.grup3.alodokter_rakamin_android_grup3.ui.forgotpassword
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Patterns
@@ -12,6 +13,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.grup3.alodokter_rakamin_android_grup3.R
 import com.grup3.alodokter_rakamin_android_grup3.base.BaseFragment
 import com.grup3.alodokter_rakamin_android_grup3.databinding.FragmentForgotPasswordBinding
+
 
 class ForgotPasswordFragment : BaseFragment<FragmentForgotPasswordBinding>() {
 
@@ -25,15 +27,33 @@ class ForgotPasswordFragment : BaseFragment<FragmentForgotPasswordBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         setupEmailListener()
-
         binding.btnSendOtp.setOnClickListener {
-            // !! Update later
+            // TODO: Add API call for OTP function here
             showSnackBar(true)
             showOTPLayout()
+            startOTPResendTimer()
+        }
+        binding.tvOtpResend.setOnClickListener {
+            // TODO: Add resend OTP function here
+            it.visibility = View.GONE
+            startOTPResendTimer()
+        }
+        binding.btnVerifyOtp.setOnClickListener {
+            // TODO: Add OTP verification function here
+
+            val resetPasswordFragment = CreateNewPasswordFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.layout_forgot_password,
+                    resetPasswordFragment
+                )
+                .addToBackStack(null)
+                .commit()
         }
     }
 
     private fun setupEmailListener() {
+        binding.btnSendOtp.isEnabled = false
         binding.edtEmail.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
@@ -44,12 +64,15 @@ class ForgotPasswordFragment : BaseFragment<FragmentForgotPasswordBinding>() {
                 when {
                     inputEmail.isEmpty() -> {
                         binding.tilEmail.error = "Email can't be empty"
+                        binding.btnSendOtp.isEnabled = false
                     }
                     Patterns.EMAIL_ADDRESS.matcher(inputEmail).matches() -> {
                         binding.tilEmail.isErrorEnabled = false
+                        binding.btnSendOtp.isEnabled = true
                     }
                     else -> {
-                        binding.tilEmail.error = "Invalid email"
+                        binding.tilEmail.error = "Invalid email address"
+                        binding.btnSendOtp.isEnabled = false
                     }
                 }
             }
@@ -60,19 +83,57 @@ class ForgotPasswordFragment : BaseFragment<FragmentForgotPasswordBinding>() {
     }
 
     private fun showSnackBar(success: Boolean) {
-        val message = if (success) getString(R.string.forgot_pass_message_otp_delivered)
-        else getString(R.string.forgot_pass_message_email_not_found)
+        val message =
+            if (success) getString(R.string.forgot_pass_message_otp_delivered)
+            else getString(R.string.forgot_pass_message_email_not_found)
 
-        val color = if (success) context?.let { ContextCompat.getColor(it, R.color.success) }
-        else context?.let { ContextCompat.getColor(it, R.color.error) }
+        val color = if (success) context?.let {
+            ContextCompat.getColor(it, R.color.success)
+        } else context?.let {
+            ContextCompat.getColor(it, R.color.error)
+        }
 
-        val sbEmailOTP = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT)
+        val sbEmailOTP = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
         color?.let { sbEmailOTP.setBackgroundTint(it) }
         sbEmailOTP.show()
     }
 
     private fun showOTPLayout() {
+        binding.edtEmail.isEnabled = false
         binding.btnSendOtp.visibility = View.GONE
         binding.llOtpField.visibility = View.VISIBLE
+        setupOtpPinViewListener()
+    }
+
+    private fun setupOtpPinViewListener() {
+        binding.btnVerifyOtp.isEnabled = false
+        binding.pvOtp.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val inputOTP = binding.pvOtp.text.toString().trim()
+                binding.btnVerifyOtp.isEnabled = inputOTP.length == 6
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+    private fun startOTPResendTimer() {
+        binding.tvOtpResendTimer.visibility = View.VISIBLE
+        object : CountDownTimer(60000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvOtpResendTimer.text =
+                    getString(R.string.forgot_pass_otp_resend_timer, millisUntilFinished / 1000)
+            }
+
+            override fun onFinish() {
+                binding.tvOtpResendTimer.visibility = View.GONE
+                binding.tvOtpResend.visibility = View.VISIBLE
+            }
+        }.start()
     }
 }
