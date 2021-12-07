23059c7f2795a5df75b0342768894b0cd6342d68
rafiannas <rafi.annas2000@gmail.com>
2021-12-07 18:58:59 +0700

    menambahkan UI change password dan detail artikel, sert memperbaiki UI signIn

---
+++ app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/ui/DetailArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.grup3.alodokter_rakamin_android_grup3.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grup3.alodokter_rakamin_android_grup3.R
+
+class DetailArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_article)
+    }
+}
