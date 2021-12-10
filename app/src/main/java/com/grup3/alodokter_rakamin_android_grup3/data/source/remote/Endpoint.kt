d894c71870e4f55e8eb570e1b5c640374a02f89c
febianbatubara <febianbatubaraa@gmail.com>
2021-12-10 19:59:51 +0700

    Add edit profile endpoint

--- app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/data/source/remote/Endpoint.kt
+++ app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/data/source/remote/Endpoint.kt
@@ -1,4 +1,15 @@
 package com.grup3.alodokter_rakamin_android_grup3.data.source.remote
 
+import com.grup3.alodokter_rakamin_android_grup3.models.body.EditProfileBody
+import retrofit2.http.Body
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
 interface Endpoint {
+
+    @PUT("users/{id}")
+    suspend fun editProfile(
+        @Body editProfileBody: EditProfileBody,
+        @Path("id") id: String
+    )
 }
