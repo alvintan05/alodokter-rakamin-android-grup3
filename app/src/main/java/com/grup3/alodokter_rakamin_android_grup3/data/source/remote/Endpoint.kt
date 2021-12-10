27b1676bfb2ba6fa28186fdef386473d3940de56
rsyd18 <rasyiddwi05@gmail.com>
2021-12-10 20:05:26 +0700

    edit data endpoint

--- app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/data/source/remote/Endpoint.kt
+++ app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/data/source/remote/Endpoint.kt
@@ -1,4 +1,11 @@
 package com.grup3.alodokter_rakamin_android_grup3.data.source.remote
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface Endpoint {
+    @GET("user/{:id}")
+    suspend fun getDetailProfile(
+        @Path("id") string id
+    )
 }
