5cbf4dd661ba97e7f780335cef8c8bc489063b5c
Alvin Tandiardi <alvintandiardi@gmail.com>
2021-12-11 15:38:43 +0700

    Change Strings  Message and Add Hide Keyboard Function
    - Change strings for password and empty
    - Add function to hidekeyboard in Base Activity
    - Move snackbar error function into Base Activity

--- app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/ui/signin/SignInActivity.kt
@@ -22,7 +22,6 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
 
     private val viewModel: SignInViewModel by viewModels()
     private lateinit var loadingDialog: AlertDialog
-    private lateinit var sbSignIn: Snackbar
 
     override fun inflateLayout(layoutInflater: LayoutInflater): ActivitySignInBinding =
         ActivitySignInBinding.inflate(layoutInflater)
@@ -43,6 +42,7 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
         }
 
         binding.btnMasuk.setOnClickListener {
+            hideKeyboard()
             checkInput()
         }
 
@@ -54,7 +54,7 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
                     finish()
                 }
                 is Resource.Error -> {
-                    resource.error?.let { setupSnackbar(it) }
+                    resource.error?.let { setupSnackbarError(it) }
                 }
             }
         })
@@ -95,12 +95,6 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
             .create()
     }
 
-    private fun setupSnackbar(message: String) {
-        sbSignIn = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT)
-            .setBackgroundTint(ContextCompat.getColor(this, R.color.error))
-        sbSignIn.show()
-    }
-
     private fun signInUser(email: String, password: String) {
         viewModel.signInUser(LoginBody(email, password))
 
@@ -115,34 +109,6 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
 
     }
 
-//    private fun setupKataSandiListener() {
-//        binding.etKataSandi.doAfterTextChanged {
-//            val inputKataSandi = it.toString().trim()
-//            when {
-//                inputKataSandi.isEmpty() -> {
-//                    binding.tlKataSandi.error = "Kata sandi tidak boleh kosong"
-//                }
-//            }
-//        }
-//    }
-
-//    private fun setupEmailListener() {
-//        binding.etEmail.doAfterTextChanged {
-//            val inputEmail = it.toString().trim()
-//            when {
-//                inputEmail.isEmpty() -> {
-//                    binding.tlEmail.error = "Email tidak boleh kosong"
-//                }
-//                Patterns.EMAIL_ADDRESS.matcher(inputEmail).matches() -> {
-//                    binding.tlEmail.isErrorEnabled = false
-//                }
-//                else -> {
-//                    binding.tlEmail.error = "Format email salah"
-//                }
-//            }
-//        }
-//    }
-
     private fun checkInput() {
         val email = binding.etEmail.text.toString().trim()
         val password = binding.etKataSandi.text.toString().trim()
@@ -150,7 +116,7 @@ class SignInActivity : BaseActivity<ActivitySignInBinding>() {
         if (viewModel.checkInput(email, password)) {
             signInUser(email, password)
         } else {
-            setupSnackbar(resources.getString(R.string.message_fix_input_data))
+            setupSnackbarError(resources.getString(R.string.message_fix_input_data))
         }
     }
 }
