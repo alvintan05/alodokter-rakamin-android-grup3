4ad1b8cdfddc60c70b0277aea7c08bbb44fee774
Alvin Tandiardi <alvintandiardi@gmail.com>
2021-12-14 13:12:57 +0700

    Change repository into interactor

--- app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/data/interactor/InteractorImpl.kt
+++ app/src/main/java/com/grup3/alodokter_rakamin_android_grup3/data/interactor/InteractorImpl.kt
@@ -4,6 +4,7 @@ import com.grup3.alodokter_rakamin_android_grup3.data.source.remote.RemoteReposi
 import com.grup3.alodokter_rakamin_android_grup3.models.Resource
 import com.grup3.alodokter_rakamin_android_grup3.models.body.EditProfileBody
 import com.grup3.alodokter_rakamin_android_grup3.models.body.LoginBody
+import com.grup3.alodokter_rakamin_android_grup3.models.body.RegisterBody
 import com.grup3.alodokter_rakamin_android_grup3.models.entity.SignInEntity
 import com.grup3.alodokter_rakamin_android_grup3.models.entity.UserEntity
 import javax.inject.Inject
@@ -15,10 +16,16 @@ class InteractorImpl @Inject constructor(
     override suspend fun signInUser(loginBody: LoginBody): Resource<SignInEntity> =
         remoteRepository.signInUser(loginBody)
 
+    override suspend fun signUpUser(registerBody: RegisterBody): Resource<UserEntity> =
+        remoteRepository.signUpUser(registerBody)
+
     override suspend fun editProfile(
         token: String,
         editProfileBody: EditProfileBody,
         id: Int
     ): Resource<UserEntity> = remoteRepository.editProfile(token, editProfileBody, id)
+
+    override suspend fun getDetailProfile(token: String, id: Int): Resource<UserEntity> =
+        remoteRepository.getDetailProfile(token, id)
 
 }
